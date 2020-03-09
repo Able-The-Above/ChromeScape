a22eff2e8abee18c8f7f7468ae12a347f00bbba0
AbleTheAbove <43394785+Angelis-Mortis@users.noreply.github.com>
2020-03-09 03:14:32 -0500

    Bruh

---
+++ src/abletheabove/chrome_scape/Main.java
@@ -0,0 +1,17 @@
+package abletheabove.chrome_scape;
+
+import abletheabove.chrome_scape.base.Window;
+
+
+public class Main {
+
+    /**
+     * Main function.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window client = new Window();
+    }
+
+}
